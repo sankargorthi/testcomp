8d7b23b675f0beed1d87e9c771c515b116169346
Sankar Gorthi <sgorthi@strongview.com>
2014-04-21 14:54:45 -0700

    Simplifies gruntfile

--- GruntFile.js
+++ GruntFile.js
@@ -1,32 +1,30 @@
 module.exports = function (grunt) {
     'use strict';
-    var hbs, component;
+    var hbs;
 
     require('load-grunt-tasks')(grunt);
     hbs = require('component-builder-handlebars');
 
-    component = {
-        options: {
-            verbose: false,
-            dev: true,
-            noRequire: false,
-            name: 'build',
-            standalone: true,
-            sourceUrls: true,
-            configure: function(builder) {
-                builder.use(hbs({
-                    extname: '.hbs',
-                    partialRegex: /^_/
-                }));
-            }
-        },
-        src: '.',
-        dest: './build/'
-    };
-
     grunt.initConfig({
         componentbuild: {
-            build: component
+            build: {
+                options: {
+                    verbose: false,
+                    dev: true,
+                    noRequire: false,
+                    name: 'build',
+                    standalone: true,
+                    sourceUrls: true,
+                    configure: function(builder) {
+                        builder.use(hbs({
+                            extname: '.hbs',
+                            partialRegex: /^_/
+                        }));
+                    }
+                },
+                src: '.',
+                dest: './build/'
+            }
         }
     });
 
--- README.md
+++ README.md
@@ -7,3 +7,14 @@ When I run grunt on this project, with the latest I get
     Warning: Cannot read property '0' of undefined Use --force to continue.
 
     Aborted due to warnings.
+
+
+##### Debugging results
+Tried to debug the build and found that the error is thrown at
+
+    setImmediate(done);
+    var list = build.components; // List `undefined` here
+    var partials = [];
+    var componentName = list[0].name; // Error thrown here
+
+I don't see the componentName variable being used anywhere either
